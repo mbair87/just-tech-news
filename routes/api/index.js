65ead47c38a1f7fa23d889147f8a427407334f6d
mbair87 <91098965+mbair87@users.noreply.github.com>
2022-01-26 17:42:10 -0700

    added post routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 
+const User = require('../../models/User.js');
+const Post = require('../../models/Post.js');
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
