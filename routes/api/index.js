468edf76efaeb73bf18fb8b8e2ce532ed6315389
mbair87 <91098965+mbair87@users.noreply.github.com>
2022-01-31 14:53:35 -0700

    finished comment routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.status(404).end();
-});
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
