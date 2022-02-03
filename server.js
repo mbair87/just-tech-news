c22745f478ceee4c677b8f8c0217f1077df720c8
mbair87 <91098965+mbair87@users.noreply.github.com>
2022-02-03 14:47:27 -0700

    added helpers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const app = express();
@@ -9,7 +10,7 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {},
